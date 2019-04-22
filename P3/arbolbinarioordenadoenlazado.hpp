e071c16b419763d2b08fbaffcda70c168ba23734
Javier Gil Moya <i72gimoj@uco.es>
2019-04-22 19:54:50 +0200

    actualizando

--- P3/arbolbinarioordenadoenlazado.hpp
+++ P3/arbolbinarioordenadoenlazado.hpp
@@ -34,46 +34,49 @@ namespace ed
 
 		public:
 			NodoArbolBinario (const G &info)
-			{
+			{//Crea un nuevo nodo sin hijos
 				this->setInfo(info);
 				_izquierdo=NULL;
 				_derecho=NULL;
 				//Postcondicion
 				#ifndef NDEBUG
+					//El nodo creado no tiene hijos
 					assert((this->getIzquierdo()==NULL) and (this->getDerecho()==NULL));
 				#endif
 			}
 
 			NodoArbolBinario (const NodoArbolBinario &n)
-			{
+			{//Constructor de copia de un nodo
+				//En el nodo nuevo se va a copiar la informacion del nodo n
 				NodoArbolBinario nuevo = new NodoArbolBinario();
 				nuevo->setInfo(n.getInfo());
 				nuevo->setIzquierdo(n.getIzquierdo());
 				nuevo->setDerecho(n.getDerecho());
 				//Postcondicion
 				#ifndef NDEBUG
+					//Los nodos son iguales
 					assert(nuevo==n);
 				#endif
 			}
 
 			/*!\brief Observadores.*/
 			const G & getInfo() const
-			{
+			{//Obtiene la informacion del nodo
 				return _info;
 			}
 
 			NodoArbolBinario *getIzquierdo() const
-			{
+			{//Obtiene el hijo izquierdo
 				return _izquierdo;
 			}
 
 			NodoArbolBinario *getDerecho() const
-			{
+			{//Obtiene el hijo derecho
 				return _derecho;
 			}
 
 			bool esHoja() const
-			{
+			{//Comprueba si el nodo tiene hijos
 				if(this->getIzquierdo()==NULL && this->getDerecho()==NULL)
 					return true;
 
@@ -81,52 +84,62 @@ namespace ed
 			}
 
 			void recorridoPreOrden (OperadorNodo<G> &operador) const
-			{
+			{//Los hijos realizan el recorrido pre-orden
+				//Mostramos la informacdion del nodo
 				operador.aplicar(this->getInfo());
+				//Si tiene hijo izquierdo, recorre desde la izquierda
 				if(this->getIzquierdo() != NULL)
 					this->getIzquierdo()->recorridoInOrden(operador);
+				//Si tiene hijo derecho, lo recorremos
 				if(this->getDerecho() != NULL)
 					this->getDerecho()->recorridoInOrden(operador);
 			}
 
 			void recorridoPostOrden (OperadorNodo<G> &operador) const
-			{
+			{//Los hijos realizanel recorrido post-orden
+				//Si tiene hijo izquierdo, recorre desde la izquierda
 				if(this->getIzquierdo() != NULL)
 					this->getIzquierdo()->recorridoInOrden(operador);
+				//Si tiene hijo derecho, lo recorremos
 				if(this->getDerecho() != NULL)
 					this->getDerecho()->recorridoInOrden(operador);
+				//Mostramos la informacdion del nodo
 				operador.aplicar(this->getInfo());
 			}
 
 			void recorridoInOrden (OperadorNodo<G> &operador) const
-			{
+			{//Los hijos realizan el recorrido en-orden
+				//Si tiene hijo izquierdo, recorre desde la izquierda
 				if(this->getIzquierdo() != NULL)
 					this->getIzquierdo()->recorridoInOrden(operador);
+				//Mostramos la informacdion del nodo
 				operador.aplicar(this->getInfo());
+				//Si tiene hijo derecho, lo recorremos
 				if(this->getDerecho() != NULL)
 					this->getDerecho()->recorridoInOrden(operador);
 			}
 
 			/*!\brief Modificadores. */
 			void setInfo(const G &info)
-			{
+			{//Establece la informacion del nodo
 				_info=info;
 			}
 
 			void setIzquierdo(NodoArbolBinario *n)
-			{
+			{//Establece el hijo izquierdo del nodo
 				_izquierdo=n;
 			}
 
 			void setDerecho(NodoArbolBinario *n)
-			{
+			{//Establece el hijo derecho del nodo
 				_derecho=n;
 			}
 
 			NodoArbolBinario & operator=(const NodoArbolBinario &n)
-			{
+			{//Asigna la informacion del nodo n al nodo actual
 				//Precondicion
 				#ifndef NDEBUG
+					//Los nodos deben ser diferentes
 					assert(this != n);
 				#endif
 				this->setInfo(n.getInfo());
@@ -136,6 +149,7 @@ namespace ed
 				return *this;
 				//Postcondicion
 				#ifndef NDEBUG
+					//LOs nodos deben ser iguales
 					assert(this == n);
 				#endif
 			}
@@ -151,70 +165,81 @@ namespace ed
 	public:
 
 		ArbolBinarioOrdenadoEnlazado ()
-		{
+		{//Creamos un arbol vacio, para ello, asignamos NULL a la raiz
 			this->_raiz=NULL;
 			//Postcondicion
 			#ifndef NDEBUG
+				//El arbol debe estar vacio
 				assert(this->estaVacio());
 			#endif
 		}
 
 		ArbolBinarioOrdenadoEnlazado (const ArbolBinarioOrdenadoEnlazado<G>& a)
-		{
+		{//Constructor de copia del arbol a al arbol actual
 			*this = a;
 			//Postcondicion
 			#ifndef NDEBUG
+				//Los arboles deben ser iguales
 				assert(this->_raiz == a._raiz);
 				assert(this->_raiz->getIzquierdo == a._raiz->getIzquierdo());
-				assert(this->_raiz->getDerecho() == a._raiz->getDerecho());	
+				assert(this->_raiz->getDerecho() == a._raiz->getDerecho());
 			#endif
 		}
 
 		~ArbolBinarioOrdenadoEnlazado ()
-		{
+		{//Destructor de la clase arbol
+			//Si el arbol esta lleno lo borra
 			if (not estaVacio())
-			borrarArbol();
+				borrarArbol();
 			cout << "Destructor Usado \n";
 		}
 
 		ArbolBinarioOrdenadoEnlazado &operator=(const ArbolBinarioOrdenadoEnlazado& a)
-		{
+		{//Asigna la informacion del arbol a al arbol actual
 			this->_raiz = a._raiz;
 			this->_actual = a._actual;
 			this->_padre = a._padre;
-			
+
 		}
 
 		bool insertar(const G &x)
-		{
+		{//Inserta un nodo en el arbol de forma ordenada
 			bool insertado=false;
-			if (this->estaVacio()){
+			if (this->estaVacio()){//Si el arbol esta vacio se inserta el nodo como raiz
 				NodoArbolBinario nuevo(x);
 				_raiz = &nuevo;
 				insertado = true;
 			}
-			else{
+			else{//Si no esta vacio se pone el cursor _actual en la raiz
 				_padre = NULL;
 				_actual = _raiz;
-			}
-			while(insertado=false){
+			}//Mientras que no se haya insertado el nodo se hara el bucle
+			while(insertado==false){
+				//Si el nodo donde se encuentra el cursor _actual es igual nodo que se quiere insertar,
+				//no se insertara el nodo
 				while(x!=_actual->getInfo()){
+					//Para insertarlo comprobamos si es menor o mayor que el nodo actual para insertarlo
+					//como hoja en el lugar correspondiente del arbol
 					if(x<_actual->getInfo()){
+						//Si es menor nos vamos al hijo izquierdo y comprobamos si tiene hijo izquiedo
 						if(_actual->getIzquierdo()!=NULL){
-							_padre=_actual;
-							_actual=_actual->getIzquierdo();
+							//Actualizamos los cursores
+							_padre=_actual;//Movemos el padre al hijo izquierdo
+							_actual=_actual->getIzquierdo();//El cursor _actual pasa a ser el hijo izquierdo
 						}
-						else{
+						else{//Si no tiene hijo izquierdo insertamos el nodo como su hijo izquierdo
 							_actual->setIzquierdo(new NodoArbolBinario(x));
 							insertado=true;
 						}
 					}
-					else{
+					else{//Si el nodo es mayor nos vamos al hijo derecho y comprobamos si tiene mas hijos derechos
 						if(_actual->getDerecho()!=NULL){
-							_padre=_actual;
-							_actual=_actual->getDerecho();
+							//Actualizamops los cursores
+							_padre=_actual;//MOvemos el poadre al hijo derecho
+							_actual=_actual->getDerecho();//El cursor _actual pasa a ser el hijo izquierdo
 						}
 						else{
+							//Si no tiene hijo derecho, insertamos el nodo como hijo derecho
 							_actual->setDerecho(new NodoArbolBinario(x));
 							insertado=true;
 						}
@@ -224,26 +249,29 @@ namespace ed
 			return insertado;
 			//Postcondicion
 			#ifndef NDEBUG
+				//Comprobamos que el nodo se encuenra en el arbol
 				assert(buscar(x));
 			#endif
 		}
 
 		void borrarArbol()
-		{
+		{//Borra el arbol por completo
 			//Precondicion
 			#ifndef NDEBUG
+				//Comprobamos que el arbol a borrar no este vacio
 				assert(!this->estaVacio());
 			#endif
 
-			_raiz = NULL;
+			_raiz = NULL;//Asignamos NULL a la raiz para indicar que el arbol se encuentra vacio
 			//Postcondicion
 			#ifndef NDEBUG
+				//Confirmamos que el arbol se ha vaciado despues de ejecutar la funcion
 				assert(this->estaVacio());
 			#endif
 		}
 
 		bool borrar()
-		{
+		{//Borra el nodo donde se encuentre el cursor _actual
 			//Precondicion
 			#ifndef NDEBUG
 				assert(existeActual());
@@ -258,13 +286,19 @@ namespace ed
 				borrado=true;
 			}
 			else{//En caso de que no sea una hoja, se borra el nodo y se cambia la estructura del arbol
+				//Si solo tiene hijo izquierdo
 				if(_actual->getDerecho()==NULL && _actual->getIzquierdo()!=NULL)
 					_padre->setDerecho(_actual->getIzquierdo());
+				//Si solo tiene hijo derecho
 				if(_actual->getIzquierdo()==NULL && _actual->getDerecho()!=NULL)
 					_padre->setIzquierdo(_actual->getDerecho());
+				//Si tiene hijo derecho e izquierdo
 				if(_actual->getDerecho()!=NULL && _actual->getIzquierdo()!=NULL){
-					_actual=_actual->getDerecho()->getIzquierdo();
-					_actual->getDerecho()->getIzquierdo()->setInfo(_actual->getDerecho()->getDerecho()->getInfo());
+					_actual=_actual->getDerecho();
+					while(_actual!=NULL){
+						_actual=_actual->getIzquierdo();
+					}
+					_actual->setInfo(_actual->getDerecho()->getDerecho()->getInfo());
 				}
 				borrado=true;
 			}
@@ -276,26 +310,26 @@ namespace ed
 		}
 
 		void recorridoPreOrden (OperadorNodo<G> &operador) const
-		{
+		{//La raiz hace el recorrido pre-orden
 			_raiz->recorridoPreOrden(operador);
 		}
 
 		void recorridoPostOrden (OperadorNodo<G> &operador) const
-		{
+		{//La raiz hace el recorrrido post-orden
 			_raiz->recorridoPostOrden(operador);
 		}
 
 		void recorridoInOrden (OperadorNodo<G> &operador) const
-		{
+		{//La raiz hace el recorrido en-orden
 			_raiz->recorridoInOrden(operador);
 		}
 
-		bool buscar(const G& x) const
+		bool buscar(const G& x)
 		{
-			// TODO
 			bool encontrado=false;
-			if(x==_raiz->getInfo())
+			if(x==_raiz->getInfo()){
 				return true;
+			}
 			_padre=NULL;
 			_actual=_raiz;
 			while(_actual->getInfo()!=x){
@@ -328,41 +362,47 @@ namespace ed
 			return encontrado;
 			//Postcondicion
 			#ifndef NDEBUG
+				//El nodo actual debe apuntar al nodo buscado
 				assert(_actual->getInfo()==x);
 			#endif
 		}
 
 		bool estaVacio() const
-		{
+		{//Comprueba si el arbol esta vacio
 			if(this->_raiz==NULL)
 				return true;
 			return false;
 		}
 
 		G raiz() const
-		{
+		{//Devuelve la informacion de la raiz
 			//Precondicion
 			#ifndef NDEBUG
+				//El arbol no puede estar vacio
 				assert(!estaVacio());
 			#endif
 			this->_raiz->getInfo();
 		}
 
 		bool existeActual() const
-		{
+		{//Comprueba si existe el nodo al cual se esta apuntado con el cursor _actual
 			//Precondicion
 			#ifndef NDEBUG
+				//Para ello el arbol no puede estar vacio
 				assert(!estaVacio());
 			#endif
-			if((this->_actual->getDerecho() != NULL) || (this->_actual->getIzquierdo() != NULL))
+			//Mientras tenga hijos o sea hoja, el nodo actual existe
+			if((this->_actual->getDerecho() != NULL) || (this->_actual->getIzquierdo() != NULL) || (this->_actual->esHoja()))
 				return true;
 			return false;
 		}
 
 		G actual() const
-		{
+		{//Devuelve la informacion del nodo donde se encuentre el cursor _actual
+			//Precondicion
 			#ifndef NDEBUG
-				assert(!existeActual());
+				//El nodo actual debe existir para poder obtener su informacion
+				assert(existeActual());
 			#endif
 			return this->_actual->getInfo();
 		}
--- P3/main.cpp
+++ P3/main.cpp
@@ -12,6 +12,56 @@ int main()
 {
   srand(time(0));
   ArbolBinarioOrdenadoEnlazado<Persona> a;
+  AlmacenarNodo<G> aux();
 
-  // TODO
+  std::cout<<"1. Insertar un numero de personas en el arbol"<<std::endl;
+  std::cout<<"2. Comprobar si una persona se encuentra en el arbol"<<std::endl;
+  std::cout<<"3. Mostrar todos los elementos del arbol. PreOrden, PostOrden, InOrden"<<std::endl;
+  std::cout<<"4. Borra una persona dando sus datos"<<std::endl;
+  std::cout<<"5. Borra el arbol completo"<<std::endl;
+
+  int x;
+  std::cout<<"Introduce la opcion que desse ejecutar"<<std::endl;
+  std::cin>>x;
+
+  switch(x){
+    case 1:
+
+      break;
+    case 2:
+      int dni;
+      std::cout<<"Introduce el dni"<<std::endl;
+      std::cin>>dni;
+      buscar(dni);
+
+      break;
+    case 3:
+      std::cout<<"Recorrido pre-orden:"<<std::endl;
+      recorridoPreOrden(&aux);
+      std::cout<<"Recorrido post-orden:"<<std::endl;
+      recorridoPostOrden(&aux);
+      std::cout<<"Recorrido en-orden:"<<std::endl;
+      recorridoInOrden(&aux);
+
+      break;
+    case 4:
+      std::cout<<"Introduce el dni"<<std::endl;
+      std::cin>>dni;
+
+      buscar(dni);
+      borrar();
+      std::cout<<"Persona borrada"<<std::endl;
+
+      break;
+    case 5:
+      borrarArbol();
+      std::cout<<"Arbol borrado"<<std::endl;
+
+      break;
+    default:
+      std::cout<<"Ha introducido un numero incorrecto"<<std::endl;
+      exit(-1);
+
+      break;
+  };
 }
