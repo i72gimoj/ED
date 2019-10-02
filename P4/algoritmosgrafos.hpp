16ad9c6e88d1ce1893b238d8bca120dcc670df4f
Javier Gil Moya <i72gimoj@uco.es>
2019-10-02 20:19:42 +0200

    ultima modificacion

--- P4/algoritmosgrafos.hpp
+++ P4/algoritmosgrafos.hpp
@@ -10,17 +10,35 @@ template <class G_Nodo, class G_Lado>
 class AlgoritmosGrafos
 {
 public:
-  // TODO
-	AlgoritmosGrafos();
+	void Floyd(G_Lado **W, G_Lado **D, G_Lado **I, int n){
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        D[i][j]=W[i][j];
+      }
+    }
 
-	/*int Distancia(){
-		Grafo<string, int> *g;
-		int dt=3;
+    for (int i = 0; i < n; i++) {
+      D[i][i]=0;
+    }
 
-		algoritmoFLoyd(g);
-		return dt;
-	}*/
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        I[i][j]=-1;
+      }
+    }
 
+    for (int k = 0; k < n; k++) {
+      for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+          if (D[i][k]+D[k][j]<D[i][j]) {
+            D[i][j]=D[i][k]+D[k][j];
+            I[i][j]=k;
+          }
+        }
+      }
+    }
+
+  }
 };
 
 #endif
--- P4/grafo.hpp
+++ P4/grafo.hpp
@@ -22,8 +22,8 @@ namespace ed
 	public:
 		// constructores
 		Grafo(){
-			_lados=NULL;
-			_nodos=NULL;
+			_lados=new G_Lado*[0];
+			_nodos=new G_Nodo[0];
 			//Postcondicion
 			#ifndef NDEBUG
 				assert(estaVacio());
@@ -31,8 +31,10 @@ namespace ed
 		}
 
 		Grafo(int n){
-			Grafo g= new Grafo();
-			g._nodos=n;
+			_nodos=new G_Nodo[n];
+			_lados=new G_Lado*[n];
+			for(int i=0; i<n; i++)
+				_lados[i]=new G_Lado[n];
 		}
 
 		Grafo(Grafo &g){
@@ -41,17 +43,30 @@ namespace ed
 
 		// destructor
 		~Grafo(){
-			/*if (not estaVacio())
-				borrarGrafo();*/
+			if (not estaVacio())
+				borrarGrafo();
 		}
 
-		/*void setLados(G_Lado **lados){
-			_lados=lados;
+		void setLados(G_Lado **&lados){
+			_lados=new G_Lado*[lados.size()];
+			for (size_t i = 0; i < sizeof(nodos); i++)
+				this->_nodos[i] = new G_Lado[sizeof(lados)];
+			for(int i=0; i<sizeof(_nodos); i++)
+				for(int j=0; j<sizeof(_nodos); j++)
+					_lados[i][j]=lados[i][j];
 		}
-
-		int getLados(){
+		G_Lado ** getLados()const{
 			return _lados;
-		}*/
+		}
+
+		void setNodos(vector<G_Nodo> *&nodos){
+			_nodos=new G_Nodo[nodos.size()];
+			for (size_t i = 0; i < nodos.size(); i++)
+				this->_nodos[i] = nodos[i];
+		}
+		G_Nodo *getNodos()const{
+			return _nodos;
+		} 
 
 		// funciones
 		void borrarGrafo() {	// cabecera indicada para que compile
@@ -60,9 +75,8 @@ namespace ed
 		}
 
 		Grafo & operator=(const Grafo &g){
-			this->_nodos=g._nodos;
-			this->_lados=g._lados;
-
+			this->setNodos(g._nodos);
+			this->setLados(g._lados);
 			return *this;
 		}
 
